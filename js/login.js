51793dc49b5a85649238c70ca14f3f48764c1e2c
perla duran <perla.duran12@gmail.com>
2021-11-10 21:04:08 -0700

    puede ser que este terminado el login

---
+++ js/login.js
@@ -0,0 +1,50 @@
+$buttonLogin = document.getElementById("button-login")
+
+
+async function apiRegistro( email, password, $loginError){
+
+ 
+    let formdata = new FormData();
+    formdata.append("email", email);
+    formdata.append("password", password);
+
+    let requestOptions = {
+        method: 'POST',
+        body: formdata,
+        redirect: 'follow'
+    };
+
+    fetch("./apis/login.php", requestOptions)
+    .then(response => response.json())
+    .then(result =>{
+
+      if(result.success==0){
+        $loginError.style.display="block";
+        
+      }else{
+        window.location.href = "base.html";
+      }
+
+
+    })
+  .catch(error => console.log('error', error)); 
+
+
+}
+
+
+
+$buttonLogin.addEventListener("click",(e)=>{
+    e.preventDefault();
+   const $email = document.getElementById("exampleInputEmail").value.trim(),
+    $password =  document.getElementById("exampleInputPassword").value.trim(),
+    $loginError = document.getElementById("register-error");
+
+   if($email =="" || $password ==""){
+       console.log("Llena todos los campos requeridos")
+   }else{
+        apiRegistro($email,$password, $loginError);
+
+   }
+
+})
--- js/registro.js
+++ js/registro.js
@@ -1,7 +1,7 @@
 $buttonRegistro = document.getElementById("button-registro")
 
 
-async function apiRegistro(name, email, password){
+ function apiRegistro(name, email, password, $registerError){
 
     var exito = false;
     var formdata = new FormData();
@@ -15,11 +15,15 @@ async function apiRegistro(name, email, password){
         redirect: 'follow'
     };
 
-    fetch("http://localhost:3000/apis/registro.php", requestOptions)
+    fetch("./apis/registro.php", requestOptions)
     .then(response => response.json())
     .then(result =>{
-      exito =  await true;
-      console.log("si entra")
+      if(result.success == 0){
+        $registerError.style.display="block";
+      }else{
+        window.location.href = "base.html";
+      }  
+      
     })
   .catch(error => console.log('error', error)); 
 
@@ -30,20 +34,17 @@ return exito;
 
 $buttonRegistro.addEventListener("click",(e)=>{
     e.preventDefault();
-   const $name = document.getElementById("exampleInputName").value.trim();
-   const $email = document.getElementById("exampleInputEmail").value.trim();
-   const $password =  document.getElementById("exampleInputPassword").value.trim();
+   const $name = document.getElementById("exampleInputName").value.trim(),
+    $email = document.getElementById("exampleInputEmail").value.trim(),
+    $password =  document.getElementById("exampleInputPassword").value.trim(),
+   $registerError = document.getElementById("login-error");
 
    if($name.trim == "" || $email =="" || $password ==""){
        console.log("Llena todos los campos requeridos")
    }else{
-       let registro = apiRegistro($name, $email,$password);
+        apiRegistro($name, $email,$password,$registerError);
 
-       if(!registro){
-           console.log("ALGO TA MAL AQUI")
-       }else{
-           console.log("TA BIEN")
-       }
+      
             
 
    }
