d7e1dd690f50ad35dd7ccae3328eb7e7ee9f2051
perla duran <perla.duran12@gmail.com>
2021-11-13 23:20:03 -0700

    Escuchar lectura casi terminado

--- js/lectura.js
+++ js/lectura.js
@@ -22,28 +22,59 @@ async function fetchGET(){
             <img src="${result.image}" alt="" class="img-fluid  col-md-8  p-5" style="width: 60%;">
             <div class="item-lectura p-5 col-xs-12 pt-9 card">
               
-              <div class="sound text-left"><i class="fas fa-volume-up"></i></div>
+              <div class="sound text-left"><i class="fas fa-volume-up " id="listen-button"></i></div>
              <div class="iconos mb-2"> <i class="fas fa-hand-point-up"></i><span>Da clic</span>
               <i class="fas fa-headphones-alt"></i><span>Escucha</span>
               <i class="fab fa-leanpub"></i><span>Aprende</span></div>
            
             
             <h1 class="mb-5"><i>${result.title}</i></h1>
-            <p class="pb-5 text-dark">${result.body}</p>  
+            <p class="pb-5 text-dark" id="lectura-texto">${result.body}</p>  
             `;
               $contenedor.classList.add("col-auto","col-xs-12", "p-5", "text-center","p-3");
 
              $cards.appendChild($contenedor);
 
-
+            listen()
 
 
         })
         .catch(error => console.log('error', error));
 
-        console.log(resultados.sinopsis)
+        
 
         
 }
 
 fetchGET();
+
+
+function listen(){
+
+ const $botonEscuchar = document.getElementById("listen-button"),
+ $texto = document.getElementById("lectura-texto").textContent ;
+    
+ const mensaje = new SpeechSynthesisUtterance();
+
+ $botonEscuchar.addEventListener("click",()=>{
+   
+  
+   $botonEscuchar.classList.toggle("volume-active");
+  console.log($botonEscuchar)
+
+    if($botonEscuchar.classList.contains( 'volume-active' )){
+ 
+      console.log("Entra aqui")
+    mensaje.lang ="en-US";
+  
+    mensaje.text = $texto;
+    window.speechSynthesis.speak(mensaje);
+  
+  }
+ })
+
+
+
+
+}
+
